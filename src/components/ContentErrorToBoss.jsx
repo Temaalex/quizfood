211fbb3ca193ca09fe9e7601ae342402e451c596
Temaalex <alekseev_artem99991@mail.ru>
2026-01-29 23:06:21 +0300

    add

--- src/components/ContentErrorToBoss.jsx
+++ src/components/ContentErrorToBoss.jsx
@@ -1,10 +1,13 @@
 import data from '../bd.json';
 import Persone from '../pictures/Persone.png';
 import useSound from 'use-sound'; 
-import ErrorSound from '../sound/gameOverLvl.mp3'
-import { useNavigate, useLocation } from "react-router-dom"
+import ErrorSound from '../sound/gameOverLvl.mp3';
+import { useNavigate, useLocation } from "react-router-dom";
+
+
 
 const ErrorBoss = () => {
+  //localStorage.setItem('check', 'false');
   let text = "Попробуем еще раз через несколько секунд?"
   const [play] = useSound(ErrorSound);
   play()
@@ -12,6 +15,37 @@ const ErrorBoss = () => {
   const next = () => {
       navigate('/toBoss')
   }
+  //таймер
+  	
+	let x=10;
+	function timer(){
+		let timer;
+		countdown();
+		function countdown(){
+			console.log(x)
+		x--
+		if (x===0){
+			clearTimeout(timer);
+			//localStorage.removeItem('check')
+			next()
+		}else{
+			timer = setTimeout (countdown, 1000);
+		}
+		}
+	}
+window.onhashchange = function() { window.location.hash = '/ErrorBoss'; }
+timer()
+  //блок 
+//   if(localStorage.getItem('check') === 'false'){
+// 	window.location.hash = '/ErrorBoss'
+// 	window.onhashchange = function() { window.location.hash = '/ErrorBoss'; }
+//   } else {
+// 	window.location.hash = '/toBoss'
+// 	window.onhashchange = function() { window.location.hash = '/toBoss'; }
+//   }
+
+  
+  
    return (
     <main>
       <div className="header">
@@ -20,8 +54,7 @@ const ErrorBoss = () => {
       </div>
       <img className="Person" src={Persone} alt="Persone"/>
     </div>
-      <div className='words'></div>
-      <button onClick={next} className="button">ВПЕРЕД</button>
+      <div className='words'>{x}</div>
     </main>
   )
   
