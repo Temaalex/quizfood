392a652984a581eeb70cf9915421b7c0cb6b760f
Temaalex <alekseev_artem99991@mail.ru>
2026-02-22 21:52:47 +0300

    add

--- src/components/TheEnd.jsx
+++ src/components/TheEnd.jsx
@@ -6,13 +6,13 @@ import data from '../bd.json';
 
 const TheEnd = () => {
   localStorage.clear()
-  const text = "ОГО! Это было очень круто! Код от сундука: 2367132"
+  // const text = "ОГО! Это было очень круто! Код от сундука: 2367132"
   return (
     <main>
       <div className="header">
       <div className="TextOfPerson">
-        {/* <p className="ConnentOfPerson">{data.contents[42].textPerson}</p> */}
-        <p className="ConnentOfPerson">{text}</p>
+        <p className="ConnentOfPerson">{data.contents[42].textPerson}</p>
+        {/* <p className="ConnentOfPerson">{text}</p> */}
       </div>
       <img className="Person"src={Person}alt="Person"/>
     </div>
